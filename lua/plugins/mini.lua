c01506bdf88e3f038538d447b03d72e3c3a3cc1d
aeonclok <posti@reimakokko.com>
2025-06-02 19:56:59 +0300

    current

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,25 @@
+return {
+  -- Add mini.nvim
+  {
+    "echasnovski/mini.nvim",
+    version = false, -- Use the latest version (or set to "stable" if preferred)
+    config = function()
+      -- Configure the mini.nvim modules you want to use
+      require("mini.surround").setup()
+      require("mini.comment").setup()
+      require("mini.pairs").setup()
+      require("mini.jump2d").setup({
+        -- Optional: customize mini.jump2d settings to mimic flash.nvim
+        labels = "abcdefghijklmnopqrstuvwxyz",
+        view = {
+          n_steps_ahead = 0, -- Immediate visualization
+        },
+      })
+    end,
+  },
+
+  -- Disable the LazyVim equivalents
+  -- { "folke/flash.nvim", enabled = false },
+  { "echasnovski/mini.surround", enabled = false }, -- If LazyVim already uses mini.surround
+  { "numToStr/Comment.nvim", enabled = false }, -- To disable LazyVim's default comment plugin
+}
--- lua/plugins/vim-vue-plugin.lua
+++
@@ -1,6 +0,0 @@
-return {}
---   {
---     "leafOfTree/vim-vue-plugin", -- Add vim-vue-plugin here
---     ft = { "vue" }, -- Optional: Lazy-load for only Vue files
---   },
--- }
